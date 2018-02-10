f500d0fa8d56118901e75227c4d464f2d98a4aa5
elizabeth <elizabeth.caronia@rutgers.edu>
2018-02-10 14:04:14 -0500

    make_video, proposal paperwork

---
+++ make_video.py
@@ -0,0 +1,46 @@
+import numpy as np
+import cv2
+
+
+def webcamvid():
+	cap = cv2.VideoCapture(0)
+
+	fourcc = cv2.VideoWriter_fourcc(*'XVID')
+	out = cv2.VideoWriter('output.avi', fourcc, 20.0, (640, 480))
+
+	while(cap.isOpened()):
+		ret, frame = cap.read()
+		if ret == True:
+			out.write(frame)
+			cv2.imshow('frame', frame)
+
+			if cv2.waitKey(1) & 0xFF == ord('q'):
+				break
+		else:
+			break
+	cap.release()
+	out.release()
+	cv2.destroyAllWindows()
+
+def watchvid():
+	cap = cv2.VideoCapture('output.avi')
+	while(cap.isOpened()):
+		ret, frame = cap.read()
+		if ret == True:
+			cv2.imshow('frame', frame)
+			if cv2.waitKey(1) & 0xFF == ord('q'):
+				break
+		else:
+			break
+	cap.release()
+	cv2.destroyAllWindows()
+
+def main():
+	choice = raw_input("0 for webcam and save, 1 for using output.avi:")
+	if choice == "0":
+		webcamvid()
+	else:
+		watchvid()
+		
+if __name__ == "__main__":
+	main()
