c39f0c86e7291750551b1456da69b32e734c2be4
elizabeth <elizabeth.caronia@rutgers.edu>
2018-03-03 16:33:00 -0500

    interface 1 made

--- make_video.py
+++ make_video.py
@@ -4,7 +4,7 @@ import cv2
 
 def webcamvid():
 	cap = cv2.VideoCapture(0)
-
+	
 	fourcc = cv2.VideoWriter_fourcc(*'XVID')
 	out = cv2.VideoWriter('output.avi', fourcc, 20.0, (640, 480))
 
@@ -12,6 +12,7 @@ def webcamvid():
 		ret, frame = cap.read()
 		if ret == True:
 			out.write(frame)
+			
 			cv2.imshow('frame', frame)
 
 			if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -24,9 +25,11 @@ def webcamvid():
 
 def watchvid():
 	cap = cv2.VideoCapture('output.avi')
+	
 	while(cap.isOpened()):
 		ret, frame = cap.read()
 		if ret == True:
+			
 			cv2.imshow('frame', frame)
 			if cv2.waitKey(1) & 0xFF == ord('q'):
 				break
