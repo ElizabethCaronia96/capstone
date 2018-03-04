58daef160d367d80e24c4619eace3b88d94ed4a0
Lawrence Maceren <lawrencemaceren96@gmail.com>
2018-03-04 15:18:18 -0500

    Make_Video.py updated for skeleton
    Skeleton now tracked in OpenCV, but for some reason 1 leg is missing.
    Also quitting does not work

--- make_video.py
+++ make_video.py
@@ -17,6 +17,42 @@ from pygame.locals import *
 KINECTEVENT = pygame.USEREVENT
 pygame.init()
 skeletons = None
+video = np.empty((480,640,4),np.uint8)
+
+SKELETON_COLORS = [(255,0,0), 
+                   (0,0,255), 
+                   (0,255,0), 
+                   (255,255,0), 
+                   (255,0,255), 
+                   (0,255,255), 
+                   (255,255,255)]
+
+LEFT_ARM = (JointId.ShoulderCenter, 
+            JointId.ShoulderLeft, 
+            JointId.ElbowLeft, 
+            JointId.WristLeft, 
+            JointId.HandLeft)
+RIGHT_ARM = (JointId.ShoulderCenter, 
+             JointId.ShoulderRight, 
+             JointId.ElbowRight, 
+             JointId.WristRight, 
+             JointId.HandRight)
+LEFT_LEG = (JointId.HipCenter, 
+            JointId.HipLeft, 
+            JointId.KneeLeft, 
+            JointId.AnkleLeft, 
+            JointId.FootLeft)
+RIGHT_LEG = (JointId.HipCenter, 
+             JointId.HipRight, 
+             JointId.KneeRight, 
+             JointId.AnkleRight, 
+             JointId.FootRight)
+SPINE = (JointId.HipCenter, 
+         JointId.Spine, 
+         JointId.ShoulderCenter, 
+         JointId.Head)
+
+skeleton_to_depth_image = nui.SkeletonEngine.skeleton_to_depth_image
 
 # Capture from Webcam
 def webcamvid():
@@ -42,19 +78,56 @@ def webcamvid():
 
 # Capture from Kinect
 def kinect_video_function(frame):
+	global video
 	video = np.empty((480,640,4),np.uint8)
 	frame.image.copy_bits(video.ctypes.data)
 
 	# Draw skeleton
-	cv2.circle(video,(300,300),10,(255,255,255))
+	draw_skeletons()
+	#cv2.circle(video,(300,300),10,(255,255,255))
 
 	# Write video to output file
 	out.write(video)
 	# Show output file
 	cv2.imshow('KINECT Video Stream', video)
 
+def draw_skeletons():
+	global skeletons
+	global video
+	if skeletons == None:
+		return
+	for index, data in enumerate(skeletons):
+		# draw the Head
+		HeadPos = skeleton_to_depth_image(data.SkeletonPositions[JointId.Head], 640, 480) 
+		draw_skeleton_data(data, index, SPINE, 10)
+
+		cv2.circle(video,(int(HeadPos[0]),int(HeadPos[1])),20,SKELETON_COLORS[index],-1)
+
+		# drawing the limbs
+		draw_skeleton_data(data, index, LEFT_ARM)
+		draw_skeleton_data(data, index, RIGHT_ARM)
+		draw_skeleton_data(data, index, LEFT_LEG)
+        draw_skeleton_data(data, index, RIGHT_LEG)
+
+def draw_skeleton_data(pSkelton, index, positions, width = 4):
+	global video
+	start = pSkelton.SkeletonPositions[positions[0]]
+		
+	for position in itertools.islice(positions, 1, None):
+		next = pSkelton.SkeletonPositions[position.value]
+
+		curstart = skeleton_to_depth_image(start, 640, 480) 
+		curend = skeleton_to_depth_image(next, 640, 480)
+
+		#pygame.draw.line(screen, SKELETON_COLORS[index], curstart, curend, width)
+		cv2.line(video,(int(curstart[0]),int(curstart[1])),(int(curend[0]),int(curend[1])),SKELETON_COLORS[index],width)
+
+		start = next
+
 def kinectvid():
 	
+	global skeletons
+	skeletons = None
 	kinect = nui.Runtime()
 	kinect.skeleton_engine.enabled = True
 
@@ -76,18 +149,31 @@ def kinectvid():
 	#out = cv2.VideoWriter('output.avi', fourcc, 30.0, (640, 480))
 
 	# Main loop - runs video_handler_function automatically
-	while True:
+	done = False
+	while done == False:
 		e = pygame.event.wait()
 
 		if e.type == KINECTEVENT:
 			skeletons = e.skeletons
 			#draw_skeletons(skeletons)
-
-		if cv2.waitKey(1) & 0xFF == ord('q'):
+		elif e.type == pygame.QUIT:
+			done = True
+			print "Quit"
 			break
 
+		if cv2.waitKey(1) & 0xFF == ord('q'):
+			done = True
+			print "Quit"
+			break
+
+	print "Exit while"
 	kinect.close()
+	print "Exit kinect"
 	cv2.destroyAllWindows()
+	print "Exit cv2"
+	pygame.quit()
+	print "Exit pygame"
+	quit()
 
 # Watch video
 def watchvid():
@@ -102,7 +188,7 @@ def watchvid():
 			break
 	cap.release()
 	cv2.destroyAllWindows()
-
+	
 # Main
 choice = raw_input("0 for webcam and save, 1 for kinect and save, 2 for playing output.avi: ")
 if choice == "0": # Webcam record
