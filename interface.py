112d700a3a3be65f7ec240d97efb45dd03d6da8e
ElizabethCaronia96 <ElizabethCaronia96@users.noreply.github.com>
2018-03-28 17:11:12 -0400

    Add files via upload
    3 buttons

--- interface.py
+++ interface.py
@@ -2,12 +2,15 @@ import pygame
 from pygame.locals import *
 import numpy as np
 import cv2
+from make_video import main, watchvid, webcamvid
+
 i = pygame.init()
 
 #COLOR DEFINITIONS
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 WHITE = (255, 255, 255)
+RED = (255, 0, 0)
 #dimensions
 tilesize = 40
 mapwidth = 10
@@ -25,68 +28,78 @@ def event_handler():
 def draw():
 	game_display.fill(WHITE)
 
-	pygame.draw.rect(game_display, GREEN, [10,10,190,380])
-	pygame.draw.rect(game_display, BLUE, [200,10,380,380])
+	pygame.draw.rect(game_display, GREEN, [10,10,185,200])
+	pygame.draw.rect(game_display, BLUE, [210,10,185,200])
+	pygame.draw.rect(game_display, RED, [10,220,385,200])
 
 
 def button(msg, x, y, w, h, ic, action):
 	mouse = pygame.mouse.get_pos()
 	click = pygame.mouse.get_pressed()
-	print click
+
 	if x+w > mouse[0] > x and y+h > mouse[1] > y:
 		pygame.draw.rect(game_display, WHITE, (x,y,w,h))
 
 		if click[0] == 1:
+			print msg
+			f = open('helloworld.txt','a')
+			f.write('\n' + msg)
+			f.close()
 			action()
+
 		
 	else:
 		pygame.draw.rect(game_display, ic, (x,y,w,h))
 
-def watchvid():
-	print 'io2'
-	cap = cv2.VideoCapture('output.avi')
+#def watchvid():
+#	print 'io2'
+#	cap = cv2.VideoCapture('output.avi')
+#
+#	while(cap.isOpened()):
+#	    ret, frame = cap.read()
+#	    if ret == True:
+#
+#		    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+#
+#		    cv2.imshow('frame',gray)
+#		    if cv2.waitKey(1) & 0xFF == ord('q'):
+#		        break
+#	    else: 
+#	    	break
+#	cap.release()
+#	cv2.destroyAllWindows()
 
-	while(cap.isOpened()):
-	    ret, frame = cap.read()
-	    if ret == True:
-
-		    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-		    cv2.imshow('frame',gray)
-		    if cv2.waitKey(1) & 0xFF == ord('q'):
-		        break
-	    else: 
-	    	break
-	cap.release()
-	cv2.destroyAllWindows()
-
-def webcamvid():
-	cap = cv2.VideoCapture(0)
-	
-	fourcc = cv2.VideoWriter_fourcc(*'XVID')
-	out = cv2.VideoWriter('output.avi', fourcc, 20.0, (640, 480))
-
-	while(cap.isOpened()):
-		ret, frame = cap.read()
-		if ret == True:
-			out.write(frame)
-			
-			cv2.imshow('frame', frame)
-
-			if cv2.waitKey(1) & 0xFF == ord('q'):
-				break
-		else:
-			break
-	cap.release()
-	out.release()
-	cv2.destroyAllWindows()
-
+#def webcamvid():
+#	cap = cv2.VideoCapture(0)
+#	
+#	fourcc = cv2.VideoWriter_fourcc(*'XVID')
+#	out = cv2.VideoWriter('output.avi', fourcc, 20.0, (640, 480))
+#
+#	while(cap.isOpened()):
+#		ret, frame = cap.read()
+#		if ret == True:
+#			out.write(frame)
+#			
+#			cv2.imshow('frame', frame)
+#
+#			if cv2.waitKey(1) & 0xFF == ord('q'):
+#				break
+#		else:
+#			break
+#	cap.release()
+#	out.release()
+#	cv2.destroyAllWindows()
+f = open('helloworld.txt','w')
+f.write('hello world')
+f.close()
 while True:
 	event_handler()
 	draw()
-	button("here", 20, 150, 150, 40, BLUE, watchvid)
-	button("here", 220, 150, 150, 40, GREEN, webcamvid)
+	button("you chose to watch a loaded video (deep pose)", 20, 75, 150, 40, BLUE,watchvid) 
+	
+	button("you chose to use your web cam (deep pose)", 220, 75, 150, 40, GREEN, webcamvid)
 
+	button("you chose to use the kinect to find the spine", 115, 320, 150, 40, GREEN, main)
 	
 
 #	smallText = pygame.font.Font("freesansbold.ttf",20)
@@ -95,4 +108,4 @@ while True:
 #	gameDisplay.blit(textSurf, textRect)
 	pygame.display.update()
 
-		
+	
